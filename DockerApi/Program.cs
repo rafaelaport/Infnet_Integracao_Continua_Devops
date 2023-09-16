74f30706c7c771e76841fa9d626a240f43265ca3
rafaelaport <rafaela.portugal96@gmail.com>
2023-09-15 23:17:08 -0300

    testando sonar

--- DockerApi/Program.cs
+++ DockerApi/Program.cs
@@ -9,8 +9,11 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
